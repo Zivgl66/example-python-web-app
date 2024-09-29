afcf27a35c729b8cbd037a2d6d70fd8e87967b59
ziv.gliser <zivgl66@gmail.com>
2024-09-29 12:13:41 +0300

    changed the title again

--- helloworld.py
+++ helloworld.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return "Hello World! Whats up?? I've made changes to the code!"
+        return "Hello World! Whats up?? I've made changes to the code again!"
 
 api.add_resource(Greeting, '/') # Route_1
 
